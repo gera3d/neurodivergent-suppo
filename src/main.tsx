7cc2789d06daaf9a7758d2076967bd17e8825388
Gera Yeremin <gera3d@gmail.com>
2025-10-30 23:45:30 +0000

    Generated by Spark: I see you did a bunch of stuff, but I don't actually see any of those components on the homepage yet.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
+import { Toaster } from 'sonner'
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
 import "./main.css"
-import "./styles/theme.css"
-import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <Toaster position="top-center" richColors />
     <App />
-   </ErrorBoundary>
+  </ErrorBoundary>
 )
